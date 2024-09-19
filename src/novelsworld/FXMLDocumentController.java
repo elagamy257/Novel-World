6295a5fa76fca379436810b08edc51c27b82554e
elagamy <https://github.com/elagamy257>
2024-09-20 02:39:53 +0300

    update dashboard

--- src/novelsworld/FXMLDocumentController.java
+++ src/novelsworld/FXMLDocumentController.java
@@ -27,14 +27,101 @@ import javafx.stage.Stage;
  */
 public class FXMLDocumentController implements Initializable {
     
-     @FXML
+    @FXML
+    private Button avl_add_btn;
+
+    @FXML
+    private TextField avl_auther;
+
+    @FXML
+    private Button avl_btn;
+
+    @FXML
+    private Button avl_clear_btn;
+
+    @FXML
+    private DatePicker avl_date;
+
+    @FXML
+    private Button avl_delete_btn;
+
+    @FXML
+    private AnchorPane avl_form;
+
+    @FXML
+    private TextField avl_id;
+
+    @FXML
+    private Button avl_import_btn;
+
+    @FXML
+    private TextField avl_price;
+
+    @FXML
+    private TextField avl_search;
+
+    @FXML
+    private TableView<?> avl_table;
+
+    @FXML
+    private TableColumn<?, ?> avl_table_auther;
+
+    @FXML
+    private TableColumn<?, ?> avl_table_date;
+
+    @FXML
+    private TableColumn<?, ?> avl_table_id;
+
+    @FXML
+    private TableColumn<?, ?> avl_table_price;
+
+    @FXML
+    private TableColumn<?, ?> avl_table_title;
+
+    @FXML
+    private TableColumn<?, ?> avl_table_type;
+
+    @FXML
+    private TextField avl_title;
+
+    @FXML
+    private TextField avl_type;
+
+    @FXML
+    private Button avl_update_btn;
+
+    @FXML
     private Button close;
+
+    @FXML
+    private Button close1;
 
     @FXML
     private Button close_signup;
 
     @FXML
     private Hyperlink create_account;
+
+    @FXML
+    private AnchorPane dash_a;
+
+    @FXML
+    private AnchorPane dash_b;
+
+    @FXML
+    private Button dash_btn;
+
+    @FXML
+    private AnchorPane dash_c;
+
+    @FXML
+    private AnchorPane dash_d;
+
+    @FXML
+    private AnchorPane dash_e;
+
+    @FXML
+    private AnchorPane dash_form;
 
     @FXML
     private TextField email_signup;
@@ -49,13 +136,76 @@ public class FXMLDocumentController implements Initializable {
     private Button loginbtn;
 
     @FXML
+    private Button logout;
+
+    @FXML
     private AnchorPane main_form;
+
+    @FXML
+    private Button minimize;
 
     @FXML
     private PasswordField password;
 
     @FXML
     private PasswordField password_signup;
+
+    @FXML
+    private Button shop_btn;
+
+    @FXML
+    private Label shopping_Total;
+
+    @FXML
+    private Button shopping_addbtn;
+
+    @FXML
+    private ComboBox<?> shopping_bookID;
+
+    @FXML
+    private ComboBox<?> shopping_bookTitle;
+
+    @FXML
+    private TableColumn<?, ?> shopping_col_auther;
+
+    @FXML
+    private TableColumn<?, ?> shopping_col_id;
+
+    @FXML
+    private TableColumn<?, ?> shopping_col_num;
+
+    @FXML
+    private TableColumn<?, ?> shopping_col_price;
+
+    @FXML
+    private TableColumn<?, ?> shopping_col_title;
+
+    @FXML
+    private TableColumn<?, ?> shopping_col_type;
+
+    @FXML
+    private AnchorPane shopping_form;
+
+    @FXML
+    private Label shopping_info_auther;
+
+    @FXML
+    private Label shopping_info_date;
+
+    @FXML
+    private Label shopping_info_id;
+
+    @FXML
+    private Label shopping_info_title;
+
+    @FXML
+    private Label shopping_info_type;
+
+    @FXML
+    private Button shopping_paybtn;
+
+    @FXML
+    private TableView<?> shopping_table;
 
     @FXML
     private AnchorPane signup_form;
@@ -69,12 +219,17 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private TextField username_signup;
 
+
     
     private Connection conn;
     private PreparedStatement ps;
     private ResultSet re;
-            
+    int x=0;
+    
+    
+    //login &signup
 public void login() {
+    
     String sql = "SELECT * FROM admin WHERE username = ? AND password = ?";
     conn = DataBase.ConDb();
     
@@ -107,12 +262,21 @@ public void login() {
                 alert.setHeaderText(null);
                 alert.setContentText("Successfully Logged in!");
                 alert.showAndWait();
+                x=1;
                 
-                Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+                if (x==1){
+                    login_form.setVisible(  false);
+                    signup_form.setVisible(false);
+                    main_form.setVisible(  true);
+                    
+                Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
                 Scene scene = new Scene(root);
                 Stage stage = new Stage();
                 stage.setScene(scene);
                 stage.show();
+                }
+                
+                
                 
                 
             } else {
@@ -182,22 +346,99 @@ public void signup() {
 
         }
     }
+    
+    //Dashboard
+public void close(){
+        System.exit(0);
+    }
+    
+public void minimize(){
+        Stage stage= (Stage)main_form.getScene().getWindow();
+        stage.setIconified(true);
+    }
+    
+public void logout(){
+    try{
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Message");
+        alert.setHeaderText(null);
+        alert.setContentText("Are you sure you want to logout?");
+        Optional<ButtonType> option = alert.showAndWait();
+
+        if(option.get().equals(ButtonType.OK)){
+
+            // HIDE YOUR DASHBOARD
+            logout.getScene().getWindow().hide();
+            login_form.setVisible(  true);
+            main_form.setVisible(  false);
+            signup_form.setVisible(false);
+            // LINK YOUR LOGIN FORM
+            Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+            Stage stage = new Stage();
+            Scene scene = new Scene(root);
 
 
 
-    public void switchForm(ActionEvent ev) {
+            stage.setScene(scene);
+            stage.show();
+        }
+
+    }catch(Exception e){e.printStackTrace();}
+}
+    
+
+
+public void switchForm(ActionEvent ev) {
         if (ev.getSource() == create_account) {
             login_form.setVisible(  false);
+            main_form.setVisible(  false);
             signup_form.setVisible(true);
         } else if (ev.getSource() == have_account) {
             login_form.setVisible(true);
             signup_form.setVisible(false);
+            main_form.setVisible(  false);
         }
     }
-    
-    public void close(){
-        System.exit(0);
+
+public void switchDash(ActionEvent event){
+
+    if(event.getSource() == dash_btn){
+        dash_form.setVisible(true);
+        avl_form.setVisible(false);
+        shopping_form.setVisible(false);
+
+        dash_btn.setStyle("-fx-background-color:linear-gradient(to top right, #72513c, #ab853e);");
+        avl_btn.setStyle("-fx-background-color: transparent");
+        shop_btn.setStyle("-fx-background-color: transparent");
+
+
+
+    }else if(event.getSource() == avl_btn){
+        dash_form.setVisible(false);
+        avl_form.setVisible(true);
+        shopping_form.setVisible(false);
+
+        avl_btn.setStyle("-fx-background-color:linear-gradient(to top right, #72513c, #ab853e);");
+        dash_btn.setStyle("-fx-background-color: transparent");
+        shop_btn.setStyle("-fx-background-color: transparent");
+
+
+
+    }else if(event.getSource() == shop_btn){
+        dash_form.setVisible(false);
+        avl_form.setVisible(false);
+        shopping_form.setVisible(true);
+
+        shop_btn.setStyle("-fx-background-color:linear-gradient(to top right, #72513c, #ab853e);");
+        avl_btn.setStyle("-fx-background-color: transparent");
+        dash_btn.setStyle("-fx-background-color: transparent");
+
+
+
     }
+}
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
